598a70f473b11fffc351f40de9f56ae928cae0f8
Fabrizzio Lopez <fabroz.dev@gmail.com>
2025-11-02 12:11:53 +0100

    feat(backend): create assignment entity

--- backend/src/main/kotlin/com/kindercentrum/planner/features/assignments/model/entity/Assignment.kt
+++ backend/src/main/kotlin/com/kindercentrum/planner/features/assignments/model/entity/Assignment.kt
@@ -1,13 +1,16 @@
 package com.kindercentrum.planner.features.assignments.model.entity
 
+import com.kindercentrum.planner.features.assignments.model.enum.DayOfWeek
 import com.kindercentrum.planner.features.children.model.entity.Child
 import com.kindercentrum.planner.features.locations.model.entity.Location
+import com.kindercentrum.planner.features.planning.model.entity.Planning
 import com.kindercentrum.planner.features.users.model.entity.User
 import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
+import java.util.*
 
 
 @Entity
@@ -16,15 +19,26 @@ import java.time.Instant
 data class Assignment(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    val id: String? = null,
+    val id: UUID? = null,
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "location_id", nullable = false, foreignKey = ForeignKey(name = "fk_assignment_location"))
     val location: Location,
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "day_of_week", nullable = false, length = 3)
+    val dayOfWeek: DayOfWeek,
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "child_id", nullable = false, foreignKey = ForeignKey(name = "fk_assignment_child"))
     val child: Child,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "planning_id", nullable = false, foreignKey = ForeignKey(name = "fk_assignment_planning"))
+    val planning: Planning,
+
+    @Column(name = "note", columnDefinition = "TEXT")
+    val note: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "created_by", foreignKey = ForeignKey(name = "fk_assignment_created_by"))
@@ -36,3 +50,5 @@ data class Assignment(
     @LastModifiedDate
     var updatedAt: Instant? = null,
 )
+
+
---
+++ backend/src/main/kotlin/com/kindercentrum/planner/features/assignments/model/enum/DayOfWeek.kt
@@ -0,0 +1,5 @@
+package com.kindercentrum.planner.features.assignments.model.enum
+
+enum class DayOfWeek {
+    MON, TUE, WED, THU, FRI
+}
