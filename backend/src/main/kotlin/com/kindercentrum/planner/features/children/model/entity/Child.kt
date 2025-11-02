97b756268412bfa3beae6382b179e7d745452694
Fabrizzio Lopez <fabroz.dev@gmail.com>
2025-11-02 12:08:42 +0100

    feat(backend): create child entity

--- backend/src/main/kotlin/com/kindercentrum/planner/features/children/model/entity/Child.kt
+++ backend/src/main/kotlin/com/kindercentrum/planner/features/children/model/entity/Child.kt
@@ -1,16 +1,11 @@
 package com.kindercentrum.planner.features.children.model.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.EntityListeners
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
+import java.util.*
 
 @Entity
 @EntityListeners(AuditingEntityListener::class)
@@ -18,7 +13,7 @@ import java.time.Instant
 data class Child(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    val id: String? = null,
+    val id: UUID? = null,
     val firstName: String,
     val lastName: String,
     @CreatedDate
