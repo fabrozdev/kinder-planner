83e143ca6fc21b320a3dcc41f6f142918e3a7b57
Fabrizzio Lopez <fabroz.dev@gmail.com>
2025-11-02 18:29:36 +0100

    feat(backend): add cors configuration to allow client

---
+++ backend/src/main/kotlin/com/kindercentrum/planner/common/configuration/WebConfiguration.kt
@@ -0,0 +1,17 @@
+package com.kindercentrum.planner.common.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
