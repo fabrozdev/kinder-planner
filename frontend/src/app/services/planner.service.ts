0b2967bfd9fa6be65284e932ffdacd47f169f9f3
Fabrizzio Lopez <fabroz.dev@gmail.com>
2025-11-03 23:39:00 +0100

    feat(frontend): create planner service

---
+++ frontend/src/app/services/planner.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Planning } from '../shared/models/planning';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlannerService {
+  private readonly http = inject(HttpClient);
+
+  getPlanning(): Observable<Planning> {
+    return this.http.get<Planning>('/planning');
+  }
+}
