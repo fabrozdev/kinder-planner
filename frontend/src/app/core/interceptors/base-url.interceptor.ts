702dc8d7c3498276a94462463b421463e015589c
Fabrizzio Lopez <fabroz.dev@gmail.com>
2025-11-03 23:38:52 +0100

    feat(frontend): create base http client configuration

---
+++ frontend/src/app/core/interceptors/base-url.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { API_BASE_URL } from '../config/api.config';
+
+export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
+  const baseUrl = inject(API_BASE_URL);
+
+  // Only prepend base URL if the request URL is relative (doesn't start with http:// or https://)
+  if (!req.url.startsWith('http://') && !req.url.startsWith('https://')) {
+    const apiReq = req.clone({
+      url: `${baseUrl}${req.url}`,
+    });
+    return next(apiReq);
+  }
+
+  return next(req);
+};
