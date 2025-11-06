28c0591a8807b164a575ea8bacac77f7bf788620
Fabrizzio Lopez <fabroz.dev@gmail.com>
2025-11-06 22:42:34 +0100

    feat(frontend): upload file dialog

--- frontend/src/app/components/settings-dialog/settings-dialog.ts
+++ frontend/src/app/components/settings-dialog/settings-dialog.ts
@@ -1,45 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
   MatDialogTitle,
 } from '@angular/material/dialog';
-import { MatButton, MatIconButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
 import { MatDivider } from '@angular/material/divider';
+import { ChildrenImport } from './components/children-import/children-import';
 
 @Component({
   selector: 'app-settings-dialog',
   imports: [
     MatDialogActions,
     MatButton,
-    MatIcon,
     MatDialogClose,
     MatDialogContent,
     MatDialogTitle,
-    MatIconButton,
     MatDivider,
+    ChildrenImport,
   ],
   templateUrl: './settings-dialog.html',
   standalone: true,
 })
-export class SettingsDialog {
-  fileName = signal<string | null>(null);
-  onFileSelected(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      const file = input.files[0];
-      this.uploadFile(file);
-    }
-  }
-
-  private uploadFile(file: File): void {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.fileName.set(file.name);
-      console.log('File uploaded:', file.name);
-    };
-    reader.readAsText(file);
-  }
-}
+export class SettingsDialog {}
