f7338a8dc6b87fb30e799520d15debdaf25a7fcf
Fabrizzio Lopez <fabroz.dev@gmail.com>
2025-11-22 21:30:36 +0100

    feat(frontend): replacing mat-chip

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -2,17 +2,23 @@ import { Component, inject, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SettingsDialog } from '@/app/components/settings-dialog/settings-dialog';
 import { PlannerNavigator } from '@/app/features/planner/components/planner-navigator/planner-navigator';
-import { MatChipListbox, MatChipOption } from '@angular/material/chips';
 import { ButtonModule } from 'primeng/button';
+import { SelectButton, SelectButtonModule } from 'primeng/selectbutton';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [PlannerNavigator, MatChipListbox, MatChipOption, ButtonModule],
+  imports: [PlannerNavigator, ButtonModule, SelectButtonModule, FormsModule],
   templateUrl: './header.html',
 })
 export class Header {
   readonly dialog = inject(MatDialog);
   mode = signal<'MONTH' | 'YEAR'>('MONTH');
+
+  modeOptions = [
+    { label: 'Monthly', value: 'MONTH' },
+    { label: 'Yearly', value: 'YEAR' },
+  ];
 
   openDialog() {
     const dialogRef = this.dialog.open(SettingsDialog);
